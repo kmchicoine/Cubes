df1d8166ee6fb254d1eb850aaa1846c7787a9809
Kaley Chicoine <kmchicoine@gmail.com>
2019-10-07 22:03:25 -0700

    neighbor lists working

--- cube.cpp
+++ cube.cpp
@@ -20,6 +20,62 @@ std::unordered_map<char, std::vector<int>> letterMap;
 
 //words: read in as set
 
+//DONE:
+//first try at algorithm
+//environment setup
+//read in files
+//create data structures
+
+//TODO:
+//make neighbor mapping
+//dfs on words
+//multithread
+//write real makefile
+//cleanup/make more c++y
+int copyNeighbs(int sheet, int cube, int offset) {
+	int i;
+	for (i = 0; i < 8 && cub[cube%16][i] >= 0; i++) {
+		cubbies[cube].neighb[i+offset] = cub[cube%16][i] + (sheet*16);
+	}
+	return i;
+}
+
+
+void SetNeighbors() {
+	//sorry this feels super hacky but it was fast to implement 
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 16; j++) {
+			int offset = copyNeighbs(i,16*i+j,0);
+			switch(i) {
+				case 0:
+					cubbies[j].neighb[offset] = 16+j;
+					copyNeighbs(1,j,offset+1);
+					break;
+				case 1:
+				case 2:
+					cubbies[16*i+j].neighb[offset] = 16*(i+1)+j;
+					cubbies[16*i+j].neighb[offset+1] = 16*(i-1)+j;
+					copyNeighbs(i+1, 16*i+j, offset+2);
+					copyNeighbs(i-1, 16*i+j, offset*2+2);
+					break;
+				case 3:
+					cubbies[48+j].neighb[offset] = 32+j;
+					copyNeighbs(2,48+j,offset+1);
+					break;
+				default:
+					std::cout << "We've got problems...";
+			}
+		}
+	}
+	for (int i = 0; i < 64; i++) {
+		std::cout << i << ": ";
+		for (int j = 0; j<26; j++) {
+			std::cout << cubbies[i].neighb[j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
 int ReadCube(FILE *f_c) {
 	char letters[65];
 	if (fscanf(f_c, "%s", letters) == EOF) {
@@ -45,6 +101,7 @@ int main(int argc, char *argv[]) {
 	//init cube struct: make neighbor list: only needs to be done once
 	//new function: read in cube file: will have to do multiple times, change sym, init used/visited=0
 	// read in word-file
+	SetNeighbors();
 	FILE *f_w = fopen(argv[2], "r");
 	if (f_w == NULL) {
 		std::cout << "unable to open word-file\n";
@@ -77,6 +134,7 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 	while (ReadCube(f_c) == 0) {
+
 		//do stuff
 	}
 
